8ee56dc0eb109bbf173420ba53d2fb8ae2abe9fd
Yashveer Bika <yashveerbika@Yashveers-MacBook-Pro.local>
2020-08-14 16:54:21 -0500

    Updates

---
+++ precache-manifest.16d70b7448393b9004cfe2008b3cc194.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef898181ab595225bd5a497dd522f16b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "affecbe5f44c98725385",
+    "url": "/static/css/main.dddc4337.chunk.css"
+  },
+  {
+    "revision": "465335b1df82d4b2dc8c",
+    "url": "/static/js/2.04749aa1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.04749aa1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "affecbe5f44c98725385",
+    "url": "/static/js/main.ca1a1a63.chunk.js"
+  },
+  {
+    "revision": "0adeb7b1d8f47cd9bf9f",
+    "url": "/static/js/runtime-main.71617d02.js"
+  },
+  {
+    "revision": "1b5a83506a16d15a96638f0e4217d1c5",
+    "url": "/static/media/monkey-picture.1b5a8350.jpg"
+  }
+]);
